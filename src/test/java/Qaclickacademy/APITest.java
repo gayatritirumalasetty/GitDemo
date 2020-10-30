aa318ba409ba82b871378524ec7513495d2f0ec5
gayatritirumalasetty <gayatritirumalasetty@gmail.com>
2020-10-30 23:24:21 +0530

    my first commit

---
+++ src/test/java/Qaclickacademy/APITest.java
@@ -0,0 +1,18 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test
+	public void JiraApi() {
+		System.out.println("JiraApi");
+	}
+	
+	@Test
+	public void GoogleApi() {
+		System.out.println("GoogleApi");
+	}
+
+}
---
+++ src/test/java/Qaclickacademy/AppiumTest.java
@@ -0,0 +1,21 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}	
+	
+	// /Users/tgayatri/Selenium_workspace_final/MavenProject
+	///Users/tgayatri/Downloads
+//	//Users/tgayatri/jenkins/MavenProject
+}
